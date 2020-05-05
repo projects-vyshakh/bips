30fed81581b64e218de9f23da15970828b71864b
vys-vital <vyshakh.vitaldesign@gmail.com>
2020-05-05 13:15:48 +0530

    from changed

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -197,8 +197,8 @@ class Attendance extends Model
             $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
             // More headers
-            $headers .= 'From: <projects.vyshakh@gmail.com>' . "\r\n";
-            $headers .= 'Cc: vyshakhps1988@gmail.com' . "\r\n";
+            $headers .= 'From: Crystalbn Networks' . "\r\n";
+            //$headers .= 'Cc: vyshakhps1988@gmail.com' . "\r\n";
 
             mail($to,$subject,$message,$headers);
 
