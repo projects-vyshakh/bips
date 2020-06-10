0510c1d698eebdc89b714fd7cec1c79c331a476c
vys-vital <vyshakh@vtldesign.com>
2020-06-10 08:06:12 +0530

    page title issue fixed

--- resources/views/layouts/content/page_title.blade.php
+++ resources/views/layouts/content/page_title.blade.php
@@ -11,13 +11,14 @@
     <div class="col-12">
 
             <div class="page-title-box">
+                @if(isset($currentScreenDetails['name']))
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item"><a href="javascript: void(0);">Home</a></li>
                         <li class="breadcrumb-item active">{{$currentScreenDetails['name']}}</li>
                     </ol>
                 </div>
-
+                @endif
                 <h4 class="page-title">
                     <div class="float-left">
                         <span class=""><i class="far fa-calendar-alt"></i>&nbsp;{{date("d-M-Y")}}</span>&nbsp;&nbsp;&nbsp;
